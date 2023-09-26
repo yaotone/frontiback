dbe6b57b9819021ee5b8b98787634f4d9ace20ab
yaotone <125026669+yaotone@users.noreply.github.com>
2023-09-26 12:43:12 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
-
+enterButtonb.onclick=function(){
+    let mailfi=document.getElementById("mail").value;
+    let passfi=document.getElementById("pass").value;
+    if(mailfi===""&&passfi===""){
+        document.getElementById("mail").className="emptyInput";
+        document.getElementById("pass").className="emptyInput";
+    }
+    else if(mailfi===""){
+        document.getElementById("mail").className="emptyInput";
+    }
+    else if(passfi===""){
+        document.getElementById("pass").className="emptyInput";
+    }
+    else if(passfi==="admin"&&mailfi==="admin@edu.mirea.ru"){
+        location.href="mainPage.html";
+    }
+}
